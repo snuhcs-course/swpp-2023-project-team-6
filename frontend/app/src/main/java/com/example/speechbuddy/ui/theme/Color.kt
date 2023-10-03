92219077d475cbaa58f356a74355c9fad6ead80a
Sukchan Lee <scleev@snu.ac.kr>
2023-10-03 17:02:58 +0900

    :lipstick: Add color theme

--- frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Color.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Color.kt
@@ -2,10 +2,39 @@ package com.example.speechbuddy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * Backgrounds
+ */
+val Green = Color(0xFFAACAAC)
+val Grey = Color(0xFFEFEFEF)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/**
+ * Text
+ */
+val Black = Color(0xFF000000)
+
+/**
+ * Title
+ */
+val Pink = Color(0xFFE03491)
+
+/**
+ * Top StatusBar
+ */
+val LightPink = Color(0xFFF1C1C1)
+
+/**
+ * Bottom MenuBar
+ */
+val Purple = Color(0xFF8872AD)
+
+/**
+ * Buttons
+ */
+val Beige = Color(0xFFE6DFA2)
+
+/**
+ * PopUp
+ */
+val Orange = Color(0xFFFFB03A)
