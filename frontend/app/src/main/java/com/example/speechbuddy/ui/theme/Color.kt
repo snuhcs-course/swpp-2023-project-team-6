91890983da042672f5accc55c7cb09d7450ab2df
Sukchan Lee <scleev@snu.ac.kr>
2023-10-07 14:05:50 +0900

    :lipstick: Apply MaterialTheme3

--- frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Color.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Color.kt
@@ -2,39 +2,64 @@ package com.example.speechbuddy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-/**
- * Backgrounds
- */
-val sb_theme_light_green = Color(0xFFADCCAF)
-val sb_theme_dark_green = Color(0xFF8FAE91)
-val sb_theme_light_grey = Color(0xFFEFEFEF)
-val sb_theme_dark_grey = Color(0xFFD1D1D1)
+val seed = Color(0xFFAACAAC)
 
-/**
- * Title
- */
-val sb_theme_title_pink = Color(0xFFE03491)
+val md_theme_light_primary = Color(0xFF0D6D35)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF9EF6AF)
+val md_theme_light_onPrimaryContainer = Color(0xFF00210B)
+val md_theme_light_secondary = Color(0xFF506352)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFD3E8D2)
+val md_theme_light_onSecondaryContainer = Color(0xFF0E1F12)
+val md_theme_light_tertiary = Color(0xFF39656E)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFBDEAF4)
+val md_theme_light_onTertiaryContainer = Color(0xFF001F25)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_outline = Color(0xFF717970)
+val md_theme_light_background = Color(0xFFFCFDF7)
+val md_theme_light_onBackground = Color(0xFF191C19)
+val md_theme_light_surface = Color(0xFFF9FAF4)
+val md_theme_light_onSurface = Color(0xFF191C19)
+val md_theme_light_surfaceVariant = Color(0xFFDDE5DA)
+val md_theme_light_onSurfaceVariant = Color(0xFF414941)
+val md_theme_light_inverseSurface = Color(0xFF2E312E)
+val md_theme_light_inverseOnSurface = Color(0xFFF0F1EC)
+val md_theme_light_inversePrimary = Color(0xFF83D995)
+val md_theme_light_surfaceTint = Color(0xFF0D6D35)
+val md_theme_light_outlineVariant = Color(0xFFC1C9BE)
+val md_theme_light_scrim = Color(0xFF000000)
 
-/**
- * Top StatusBar
- */
-val sb_theme_light_pink = Color(0xFFF1C1C1)
-val sb_theme_dark_pink = Color(0xFFC9A3A3)
-
-/**
- * Bottom MenuBar
- */
-val sb_theme_light_purple = Color(0xFF8872AD)
-val sb_theme_dark_purple = Color(0xFF6A548F)
-
-/**
- * Buttons
- */
-val sb_theme_light_beige = Color(0xFFE6DFA2)
-val sb_theme_dark_beige = Color(0xFFC8C184)
-
-/**
- * PopUp
- */
-val sb_theme_light_orange = Color(0xFFFFB03A)
-val sb_theme_dark_orange = Color(0xFFE1921C)
+val md_theme_dark_primary = Color(0xFF83D995)
+val md_theme_dark_onPrimary = Color(0xFF003918)
+val md_theme_dark_primaryContainer = Color(0xFF005225)
+val md_theme_dark_onPrimaryContainer = Color(0xFF9EF6AF)
+val md_theme_dark_secondary = Color(0xFFB7CCB7)
+val md_theme_dark_onSecondary = Color(0xFF233426)
+val md_theme_dark_secondaryContainer = Color(0xFF394B3B)
+val md_theme_dark_onSecondaryContainer = Color(0xFFD3E8D2)
+val md_theme_dark_tertiary = Color(0xFFA2CED8)
+val md_theme_dark_onTertiary = Color(0xFF00363E)
+val md_theme_dark_tertiaryContainer = Color(0xFF204D55)
+val md_theme_dark_onTertiaryContainer = Color(0xFFBDEAF4)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_outline = Color(0xFF8B9389)
+val md_theme_dark_background = Color(0xFF191C19)
+val md_theme_dark_onBackground = Color(0xFFE2E3DE)
+val md_theme_dark_surface = Color(0xFF111411)
+val md_theme_dark_onSurface = Color(0xFFC5C7C2)
+val md_theme_dark_surfaceVariant = Color(0xFF414941)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC1C9BE)
+val md_theme_dark_inverseSurface = Color(0xFFE2E3DE)
+val md_theme_dark_inverseOnSurface = Color(0xFF191C19)
+val md_theme_dark_inversePrimary = Color(0xFF0D6D35)
+val md_theme_dark_surfaceTint = Color(0xFF83D995)
+val md_theme_dark_outlineVariant = Color(0xFF414941)
+val md_theme_dark_scrim = Color(0xFF000000)
--- frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Type.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/ui/theme/Type.kt
@@ -2,66 +2,100 @@ package com.example.speechbuddy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.speechbuddy.R
-
-val manropeFamily = FontFamily(
-    Font(R.font.Manrope_Bold, FontWeight.Bold, FontStyle.Normal),
-    Font(R.font.Manrope_ExtraBold, FontWeight.ExtraBold, FontStyle.Normal),
-    Font(R.font.Manrope_SemiBold, FontWeight.SemiBold, FontStyle.Normal),
-    Font(R.font.Manrope_Regular, FontWeight.Normal, FontStyle.Normal),
-    Font(R.font.Manrope_Light, FontWeight.Light, FontStyle.Normal),
-    Font(R.font.Manrope_ExtraLight, FontWeight.ExtraLight, FontStyle.Normal),
-    Font(R.font.Manrope_Medium, FontWeight.Medium, FontStyle.Normal)
-)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    // h1
     displayLarge = TextStyle(
-        fontFamily = manropeFamily,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
-        fontSize = 32.sp,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
     ),
-    // h2
     displayMedium = TextStyle(
-        fontFamily = manropeFamily,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
     ),
-    // h3
     displaySmall = TextStyle(
-        fontFamily = manropeFamily,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
     ),
-    // h4
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+    ),
     headlineMedium = TextStyle(
-        fontFamily = manropeFamily,
-        fontWeight = FontWeight.Bold,
-        fontSize = 16.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
     ),
-    // h5
     headlineSmall = TextStyle(
-        fontFamily = manropeFamily,
-        fontWeight = FontWeight.Bold,
-        fontSize = 12.sp,
-    ),
-    // h2_regular
-    bodyMedium = TextStyle(
-        fontFamily = manropeFamily,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
         fontSize = 24.sp,
+        lineHeight = 32.sp,
     ),
-    // h4_regular
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
     bodySmall = TextStyle(
-        fontFamily = manropeFamily,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
+        lineHeight = 16.sp,
     ),
-
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+    ),
 )
