3afbd492d01c04702e6fe1dbca7ae4a80c360a13
Sukchan Lee <scleev@snu.ac.kr>
2023-10-08 21:46:17 +0900

    :sparkle: Add surface to LandingScreen

--- frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
@@ -1,10 +1,28 @@
 package com.example.speechbuddy.compose.landing
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.speechbuddy.ui.SpeechBuddyTheme
 
 @Composable
 fun LandingScreen(
+    modifier: Modifier = Modifier,
     onLoginClick: () -> Unit,
 ) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primaryContainer
+    ) {}
+}
 
+@Preview
+@Composable
+private fun LandingScreenPreview() {
+    SpeechBuddyTheme {
+        LandingScreen(onLoginClick = {})
+    }
 }
