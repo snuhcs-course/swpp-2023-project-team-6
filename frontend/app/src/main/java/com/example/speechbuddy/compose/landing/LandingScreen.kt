fb17643d02d9e60a5562c3d0a937cbaecc573abe
Sukchan Lee <scleev@snu.ac.kr>
2023-10-08 23:08:32 +0900

    :sparkles: Implement landing screen

--- frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
@@ -1,11 +1,22 @@
 package com.example.speechbuddy.compose.landing
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.speechbuddy.R
 import com.example.speechbuddy.ui.SpeechBuddyTheme
 
 @Composable
@@ -13,10 +24,24 @@ fun LandingScreen(
     modifier: Modifier = Modifier,
     onLoginClick: () -> Unit,
 ) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.primaryContainer
-    ) {}
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primaryContainer) {
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+            contentDescription = stringResource(id = R.string.app_name),
+        )
+        Column(
+            modifier = Modifier.padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Button(onClick = { onLoginClick() }) {
+                Text("Login")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Guest Mode")
+            }
+        }
+    }
 }
 
 @Preview
