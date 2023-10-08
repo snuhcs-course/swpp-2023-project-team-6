7c9215a556c8d8bd8c83128bfefb71d73ba159fa
MyungHyun Ryu <paulryu@snu.ac.kr>
2023-10-08 22:50:51 +0900

    :wrench: Add configuration for navigation in build.gradle

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +66,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Navigation for compose
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    // Navigation for Kotlin
+    val nav_version = "2.5.3"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
 }
