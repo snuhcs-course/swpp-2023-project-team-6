727a02c6a014d595f5b0e3c5854dc7594f0475fb
Sukchan Lee <scleev@snu.ac.kr>
2023-10-09 12:31:15 +0900

    :sparkles: Implement button UI

--- frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/landing/LandingScreen.kt
@@ -2,13 +2,12 @@ package com.example.speechbuddy.compose.landing
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +16,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.speechbuddy.R
+import com.example.speechbuddy.compose.utils.ButtonUi
 import com.example.speechbuddy.ui.SpeechBuddyTheme
 
 @Composable
@@ -24,21 +24,22 @@ fun LandingScreen(
     modifier: Modifier = Modifier,
     onLoginClick: () -> Unit,
 ) {
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primaryContainer) {
+    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primaryContainer) {
         Image(
             painter = painterResource(id = R.mipmap.ic_launcher_foreground),
             contentDescription = stringResource(id = R.string.app_name),
         )
-        Column(
-            modifier = Modifier.padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
+        Box(
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 64.dp),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            Button(onClick = { onLoginClick() }) {
-                Text("Login")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Guest Mode")
+            Column(
+                verticalArrangement = Arrangement.spacedBy(14.dp)
+            ) {
+                ButtonUi(
+                    text = stringResource(id = R.string.guess_mode_action),
+                    onClick = { /*TODO*/ })
+                ButtonUi(text = stringResource(id = R.string.login_action), onClick = onLoginClick)
             }
         }
     }
---
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/utils/ButtonUi.kt
@@ -0,0 +1,49 @@
+package com.example.speechbuddy.compose.utils
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.speechbuddy.ui.SpeechBuddyTheme
+
+@Composable
+fun ButtonUi(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    error: Boolean = false,
+) {
+    val containerColor: Color = when (error) {
+        true -> MaterialTheme.colorScheme.error
+        else -> MaterialTheme.colorScheme.primary
+    }
+
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth().height(44.dp),
+        enabled = enabled,
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(containerColor)
+    ) {
+        Text(text = text, style = MaterialTheme.typography.titleMedium)
+    }
+}
+
+@Preview
+@Composable
+fun ButtonUiPreview() {
+    SpeechBuddyTheme {
+        ButtonUi(text = "커스텀 버튼", onClick = {})
+    }
+}
