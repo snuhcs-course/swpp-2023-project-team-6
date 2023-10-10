f5902d3b12a24105d7b44db6f60d03815ff547e3
Sukchan Lee <scleev@snu.ac.kr>
2023-10-10 16:02:11 +0900

    :art: Optimize imports & Rename SignupViewModel

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3n")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,11 +69,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    // Navigation for compose
-    implementation("androidx.navigation:navigation-compose:2.5.3")
-    // Navigation for Kotlin
-    val nav_version = "2.5.3"
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-
+    
 }
--- frontend/app/src/main/java/com/example/speechbuddy/compose/utils/ButtonUi.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/utils/ButtonUi.kt
@@ -2,7 +2,6 @@ package com.example.speechbuddy.compose.utils
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -11,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.speechbuddy.ui.SpeechBuddyTheme
@@ -31,7 +29,9 @@ fun ButtonUi(
 
     Button(
         onClick = onClick,
-        modifier = modifier.fillMaxWidth().height(44.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(44.dp),
         enabled = enabled,
         shape = RoundedCornerShape(10.dp),
         colors = ButtonDefaults.buttonColors(containerColor)
