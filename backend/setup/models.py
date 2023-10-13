dadd6c67fff36116c4a0fe85cd0d4c5efd22dea0
YeonJeong Kim <kyj219@snu.ac.kr>
2023-10-14 01:21:40 +0900

    :recycle: Update models in Setup and Entry apps

--- backend/setup/models.py
+++ backend/setup/models.py
@@ -13,7 +13,7 @@ class Setting(models.Model):
         (1, 'tts'),
     )
 
-    user = models.ForeignKey('user.User', related_name='settings', on_delete=models.CASCADE)
+    user = models.OneToOneField('user.User', related_name='settings', on_delete=models.CASCADE)
     display_mode = models.IntegerField(null=False, default=0, choices=DISPLAY_CHOICE)
     default_menu = models.IntegerField(null=False, default=0, choices=MENU_CHOICE)
 
