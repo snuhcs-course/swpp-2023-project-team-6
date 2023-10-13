8f026aad74984aa74bee9876e5c54055f69e1e74
MyungHyun Ryu <paulryu@snu.ac.kr>
2023-10-13 17:52:36 +0900

    :art: Update Ui using util composable

--- frontend/app/src/main/java/com/example/speechbuddy/compose/login/LoginScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/login/LoginScreen.kt
@@ -1,40 +1,31 @@
 package com.example.speechbuddy.compose.login
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.speechbuddy.R
+import com.example.speechbuddy.compose.utils.ButtonLevel
 import com.example.speechbuddy.compose.utils.ButtonUi
+import com.example.speechbuddy.compose.utils.TextFieldUi
+import com.example.speechbuddy.compose.utils.TitleUi
+import com.example.speechbuddy.compose.utils.TopAppBarUi
+import com.example.speechbuddy.ui.SpeechBuddyTheme
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -43,36 +34,19 @@ import com.example.speechbuddy.compose.utils.ButtonUi
 fun LoginScreen(
     onSignupClick: () -> Unit,
     onLandingClick: () -> Unit,
+    onResetPasswordClick: () -> Unit
 ) {
     Surface(
         modifier = Modifier
             .fillMaxSize()
     ) {
+
         Scaffold(
             modifier = Modifier
                 .fillMaxSize(),
             topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Image(
-                            painter = painterResource(id = R.drawable.top_app_bar_ic),
-                            contentDescription = "app logo",
-                            contentScale = ContentScale.Fit,
-                            modifier = Modifier.size(148.dp)
-                        )
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = {
-                            /*TODO*/
-                            onLandingClick()
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Go back to landing page"
-                            )
-                        }
-                    },
-//                    scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+                TopAppBarUi(
+                    onBackClick = { onLandingClick() }
                 )
             }
         ) {
@@ -86,80 +60,64 @@ fun LoginScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
             ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Start),
-                    text = stringResource(id = R.string.login_text),
-                    style = MaterialTheme.typography.displayMedium,
-                )
-
-                Text(
-                    modifier = Modifier.align(Alignment.Start),
-                    text = stringResource(id = R.string.login_explain),
-                    style = MaterialTheme.typography.bodyMedium,
+                TitleUi(
+                    title = stringResource(id = R.string.login_text),
+                    description = stringResource(id = R.string.login_explain)
                 )
 
                 Spacer(modifier = Modifier.height(15.dp))
 
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth(),
+                // Email Text Field
+                TextFieldUi(
+                    label = { Text(stringResource(id = R.string.email_field)) },
                     value = username.value,
                     onValueChange = { username.value = it },
-                    label = { Text(stringResource(id = R.string.email_field)) },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = MaterialTheme.colorScheme.error
-                    )
-//                keyboardOptions = Default.copy(imeAction = ImeAction.Next),
-//                keyboardActions = KeyboardActions(onNext = { /* focus next field */ })
+                    supportingText = { Text(stringResource(id = R.string.false_email)) },
+                    isError = false,
+                    isValid = false,
+                    isHidden = false,
                 )
 
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Start)
-                        .height(25.dp),
-                    text = stringResource(id = R.string.false_email),
-                    color = MaterialTheme.colorScheme.error,
-                    style = MaterialTheme.typography.bodySmall,
-                )
-
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth(),
+                // Password Text Field
+                TextFieldUi(
+                    label = { Text(stringResource(id = R.string.password_field)) },
                     value = password.value,
                     onValueChange = { password.value = it },
-                    label = { Text(stringResource(id = R.string.password_field)) },
-                    // when keyboard action
-//                keyboardOptions = Default.copy(imeAction = ImeAction.Done),
-//                keyboardActions = KeyboardActions(onDone = { /* perform login */ })
+                    supportingText = { Text(stringResource(id = R.string.false_password)) },
+                    isError = false,
+                    isValid = false,
+                    isHidden = false,
                 )
 
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Start)
-                        .height(40.dp),
-                    text = stringResource(id = R.string.false_password),
-                    color = MaterialTheme.colorScheme.error,
-                    style = MaterialTheme.typography.bodySmall,
-                    textAlign = TextAlign.Center,
-                )
-
+                // Login Button
                 ButtonUi(
                     text = stringResource(id = R.string.login_text),
                     onClick = { /* perform login */ },
+                    isError = false,
+                    isEnabled = true,
+                    level = ButtonLevel.PRIMARY
                 )
 
                 Spacer(modifier = Modifier.height(8.dp))
 
+                // Forgot Password Button
                 ButtonUi(
                     text = stringResource(id = R.string.forgot_passowrd),
-                    onClick = { /*forgot password*/ },
+                    onClick = { onResetPasswordClick() },
+                    isError = false,
+                    isEnabled = true,
+                    level = ButtonLevel.PRIMARY
                 )
 
                 Spacer(modifier = Modifier.height(145.dp))
 
+                // Signup Button
                 ButtonUi(
                     text = stringResource(id = R.string.signup),
-                    onClick = { /* sign up */ },
+                    onClick = { onSignupClick() },
+                    isError = false,
+                    isEnabled = true,
+                    level = ButtonLevel.PRIMARY
                 )
             }
         }
@@ -170,6 +128,8 @@ fun LoginScreen(
 @Preview
 @Composable
 private fun LoginScreenPreview() {
-    LoginScreen(onLandingClick = {}, onSignupClick = {})
+    SpeechBuddyTheme {
+        LoginScreen(onLandingClick = {}, onSignupClick = {}, onResetPasswordClick = {})
+    }
 }
 
