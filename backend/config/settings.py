bb48d59d736f244b3aa3beb6d84221cf591f5d1c
JH <chess109@naver.com>
2023-10-13 18:07:01 +0900

    :sparkles: Add Setting, Entry apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,7 +73,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework.authtoken',
-    'user'
+    'user',
+    'setting',
+    'entry'
 ]
 
 MIDDLEWARE = [
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('setting/', include('setting.urls')),
+    path('symbol/', include('entry.urls')),
 ]
