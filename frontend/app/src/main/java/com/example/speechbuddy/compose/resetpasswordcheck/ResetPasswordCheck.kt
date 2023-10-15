f3e28d103654ef2e44d14c56f0c2c7a8c3ad5dcb
류명현 <paulryu@snu.ac.kr>
2023-10-15 20:23:59 +0900

    :art: Adjust to code convention
    Co-authored-by: 이석찬 / Lee Sukchan <scleev@snu.ac.kr>

--- frontend/app/src/main/java/com/example/speechbuddy/compose/resetpasswordcheck/ResetPasswordCheck.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/resetpasswordcheck/ResetPasswordCheck.kt
@@ -43,7 +43,7 @@ fun ResetPasswordCheck(
         Scaffold(
             topBar = {
                 TopAppBarUi(
-                    onBackClick = { onLoginClick() }
+                    onBackClick = onBackClick
                 )
             }
         ) {
