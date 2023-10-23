7b67e50735f202e006c4900545a91a3d6d289dc1
MyungHyun Ryu <paulryu@snu.ac.kr>
2023-10-23 18:34:44 +0900

    :wrench: Update Base url

--- frontend/app/src/main/java/com/example/speechbuddy/utils/Constants.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.speechbuddy.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://10.0.2.2:8000/"
+        const val BASE_URL = "http://54.180.112.72:8000/"
         const val MINIMUM_PASSWORD_LENGTH = 8
     }
 }
