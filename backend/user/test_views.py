8d13c2ee4068d0baf3260a8e63da3a76c20f14b9
YeonJeong Kim <kyj219@snu.ac.kr>
2023-10-25 17:07:19 +0900

    :white_check_mark: Add tentative test functions to trigger CD

--- backend/user/test_views.py
+++ backend/user/test_views.py
@@ -4,3 +4,28 @@ from unittest import TestCase
 class TestUserSignUpView(TestCase):
     def test_post(self):
         self.fail()
+
+
+class TestSignUpEmailVerifySendView(TestCase):
+    def test_post(self):
+        self.fail()
+
+
+class TestPasswordEmailVerifySendView(TestCase):
+    def test_post(self):
+        self.fail()
+
+
+class TestSignUpEmailVerifyAcceptView(TestCase):
+    def test_post(self):
+        self.fail()
+
+
+class TestPasswordEmailVerifyAcceptView(TestCase):
+    def test_post(self):
+        self.fail()
+
+
+class TestUserLoginView(TestCase):
+    def test_post(self):
+        self.fail()
