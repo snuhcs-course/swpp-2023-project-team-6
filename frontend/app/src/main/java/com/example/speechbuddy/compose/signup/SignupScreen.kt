86102fdcf333c983c71672f1e440b373fdffba35
MyungHyun Ryu <paulryu@snu.ac.kr>
2023-10-25 22:44:53 +0900

    :fire: Remove unused imports

--- frontend/app/src/main/java/com/example/speechbuddy/compose/signup/SignupScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/signup/SignupScreen.kt
@@ -1,29 +1,22 @@
 package com.example.speechbuddy.compose.signup
 
 import android.annotation.SuppressLint
-import com.example.speechbuddy.compose.utils.AlertDialogUi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.speechbuddy.R
@@ -31,10 +24,7 @@ import com.example.speechbuddy.compose.utils.ButtonUi
 import com.example.speechbuddy.compose.utils.TextFieldUi
 import com.example.speechbuddy.compose.utils.TitleUi
 import com.example.speechbuddy.compose.utils.TopAppBarUi
-import com.example.speechbuddy.ui.SpeechBuddyTheme
 import com.example.speechbuddy.ui.models.SignupErrorType
-import com.example.speechbuddy.utils.Resource
-import com.example.speechbuddy.utils.Status
 import com.example.speechbuddy.viewmodel.SignupViewModel
 
 
