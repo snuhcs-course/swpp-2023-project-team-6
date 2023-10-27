f88695d8fdd1d559b24b9c3447322c310e3c66c1
YeonJeong Kim <kyj219@snu.ac.kr>
2023-10-27 15:46:28 +0900

    :card_file_box: Add image and is_valid field to symbol model

--- backend/entry/models.py
+++ backend/entry/models.py
@@ -1,15 +1,30 @@
+import os
+from django.utils import timezone
+from uuid import uuid4
+
 from django.db import models
 
 from user.models import User
 
 
-# Create your models here.
+# 저장 경로 설정
+def img_upload_func(instance, filename):
+    prefix = 'symbol/user_{}/'.format(instance.created_by.id)
+    ymd_path = timezone.now().strftime('%Y%m%d')
+    file_name = uuid4().hex  # random string
+    extension = os.path.splitext(filename)[-1].lower()  # 확장자 추출
+    return "".join(
+        [prefix, ymd_path, file_name, extension, ]
+    )
+
+
 class Symbol(models.Model):
     text = models.CharField(max_length=15, null=False, blank=False)
     category = models.ForeignKey('Category', related_name='symbols', on_delete=models.CASCADE) # Would it be better to use SET_NULL/DEFAULT?
-    # image = models.ImageField
+    image = models.ImageField(blank=True, upload_to=img_upload_func)
     created_by = models.ForeignKey('user.User', related_name='symbols', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_valid = models.BooleanField(default=False)
 
 
 class FavoriteSymbol(models.Model):
