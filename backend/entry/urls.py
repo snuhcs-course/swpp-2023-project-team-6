641ac13f3ff7bf6c3b7fed10bf5fcedef56e033c
Sukchan Lee <scleev@snu.ac.kr>
2023-11-01 15:50:51 +0900

    :card_file_box: Modify symnbol and category ids to start from 1

--- backend/entry/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'entry'
-urlpatterns = [
-    path('favorite/backup/', views.FavoriteBackupView.as_view(), name='backup favorite symbol'),
-]
