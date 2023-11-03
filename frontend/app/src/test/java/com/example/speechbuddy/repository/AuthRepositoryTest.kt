eab7645fee80fe21f9c9d8847a050de279450091
YeonJeong Kim <kyj219@snu.ac.kr>
2023-11-03 12:48:40 +0900

    :white_check_mark: Implement testSignupSuccess test for AuthRepository test

---
+++ frontend/app/src/test/java/com/example/speechbuddy/repository/AuthRepositoryTest.kt
@@ -0,0 +1,49 @@
+package com.example.speechbuddy.repository
+
+import com.example.speechbuddy.data.remote.AuthTokenRemoteSource
+import com.example.speechbuddy.data.remote.models.AuthTokenDtoMapper
+import com.example.speechbuddy.data.remote.requests.AuthSignupRequest
+import com.example.speechbuddy.utils.Status
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Response
+
+class AuthRepositoryTest {
+
+    private lateinit var authRepository: AuthRepository
+    private val authTokenRemoteSource = mockk<AuthTokenRemoteSource>()
+    private val authTokenDtoMapper = AuthTokenDtoMapper()
+
+    @Before
+    fun setup() {
+        authRepository = AuthRepository(authTokenRemoteSource, authTokenDtoMapper)
+    }
+
+    @Test
+    fun testSignupSuccess() {
+        val authSignupRequest = AuthSignupRequest(
+            email = "testemail@google.com",
+            nickname = "testnickname",
+            password = "testpassword"
+        )
+        // 성공 케이스의 Response<Void>를 정의
+        val successResponse = Response.success<Void>(201, null)
+
+        coEvery { authTokenRemoteSource.signupAuthToken(authSignupRequest) } returns flowOf(successResponse)
+
+        runBlocking {
+            val result = authRepository.signup(authSignupRequest)
+            // 아래 resource는 Resource<Void> 타입
+            result.collect { resource ->
+                assert(resource.status == Status.SUCCESS)
+                assert(resource.data == null)
+                assert(resource.message == null)
+            }
+        }
+    }
+}
+
