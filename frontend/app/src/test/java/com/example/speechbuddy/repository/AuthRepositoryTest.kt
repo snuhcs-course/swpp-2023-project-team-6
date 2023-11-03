9f8d22ce52880417757c714b5542a8b2b0fa07b6
YeonJeong Kim <kyj219@snu.ac.kr>
2023-11-03 13:14:06 +0900

    :white_check_mark: Implement testVerifySendSignupSuccess test for AuthRepository test

--- frontend/app/src/test/java/com/example/speechbuddy/repository/AuthRepositoryTest.kt
+++ frontend/app/src/test/java/com/example/speechbuddy/repository/AuthRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.example.speechbuddy.repository
 import com.example.speechbuddy.data.remote.AuthTokenRemoteSource
 import com.example.speechbuddy.data.remote.models.AuthTokenDtoMapper
 import com.example.speechbuddy.data.remote.requests.AuthSignupRequest
+import com.example.speechbuddy.data.remote.requests.AuthVerifyEmailSendRequest
 import com.example.speechbuddy.utils.Status
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -25,18 +26,41 @@ class AuthRepositoryTest {
 
     @Test
     fun testSignupSuccess() {
-        val authSignupRequest = AuthSignupRequest(
-            email = "testemail@google.com",
-            nickname = "testnickname",
-            password = "testpassword"
-        )
-        // 성공 케이스의 Response<Void>를 정의
-        val successResponse = Response.success<Void>(201, null)
-
-        coEvery { authTokenRemoteSource.signupAuthToken(authSignupRequest) } returns flowOf(successResponse)
-
         runBlocking {
+            val authSignupRequest = AuthSignupRequest(
+                email = "testemail@google.com",
+                nickname = "testnickname",
+                password = "testpassword"
+            )
+            // 성공 케이스의 Response<Void>를 정의
+            val successResponse = Response.success<Void>(201, null)
+
+            coEvery { authTokenRemoteSource.signupAuthToken(authSignupRequest) } returns flowOf(successResponse)
+
             val result = authRepository.signup(authSignupRequest)
+            // 아래 resource는 Resource<Void> 타입
+            result.collect { resource ->
+                assert(resource.status == Status.SUCCESS)
+                assert(resource.data == null)
+                assert(resource.message == null)
+            }
+        }
+    }
+
+//    @Test
+//    fun testLoginSuccess(){}
+
+    @Test
+    fun testVerifySendSignupSuccess() {
+        runBlocking {
+            val authVerifyEmailSendRequest = AuthVerifyEmailSendRequest(
+                email = "testemail@google.com"
+            )
+            val successResponse = Response.success<Void>(200, null)
+
+            coEvery { authTokenRemoteSource.verifySendSignupAuthToken(authVerifyEmailSendRequest) } returns flowOf(successResponse)
+
+            val result = authRepository.verifySendSignup(authVerifyEmailSendRequest)
             // 아래 resource는 Resource<Void> 타입
             result.collect { resource ->
                 assert(resource.status == Status.SUCCESS)
