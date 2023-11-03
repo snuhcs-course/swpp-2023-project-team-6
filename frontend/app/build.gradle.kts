06c290bdfb74a8a6212e407fdd9e7406bae71ff8
Sukchan Lee <scleev@snu.ac.kr>
2023-11-03 15:05:47 +0900

    :rotating_light: Add opt in annotations & rearrange dependencies

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -83,6 +84,7 @@ dependencies {
     val coroutinesVersion = "1.7.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 
     // Preference
     implementation("androidx.preference:preference-ktx:1.2.1")
@@ -119,10 +121,8 @@ dependencies {
 
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-    // Test
+    // MockK
     val mockkVersion = "1.13.8"
-    testImplementation ("io.mockk:mockk-android:$mockkVersion")
-    testImplementation ("io.mockk:mockk-agent:$mockkVersion")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
-    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("io.mockk:mockk-agent:$mockkVersion")
+    testImplementation("io.mockk:mockk-android:$mockkVersion")
 }
