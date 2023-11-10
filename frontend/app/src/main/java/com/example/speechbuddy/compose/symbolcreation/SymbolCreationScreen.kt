3823b1229a1fb90b081faea86ac5bcb057e34b39
=YeonJeong Kim <kyj219@snu.ac.kr>
2023-11-10 16:10:02 +0900

    :sparkles: Implement SymbolCreationScreen and SymbolCreationViewmodel

--- frontend/app/src/main/java/com/example/speechbuddy/compose/symbolcreation/SymbolCreationScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/symbolcreation/SymbolCreationScreen.kt
@@ -1,43 +1,91 @@
 package com.example.speechbuddy.compose.symbolcreation
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.speechbuddy.R
+import com.example.speechbuddy.compose.utils.ButtonUi
 import com.example.speechbuddy.compose.utils.HomeTopAppBarUi
+import com.example.speechbuddy.compose.utils.TextFieldUi
 import com.example.speechbuddy.compose.utils.TitleUi
+import com.example.speechbuddy.domain.models.Category
+import com.example.speechbuddy.ui.models.SymbolCreationErrorType
+import com.example.speechbuddy.viewmodel.SymbolCreationViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SymbolCreationScreen(
     modifier: Modifier = Modifier,
-    bottomPaddingValues: PaddingValues
+    bottomPaddingValues: PaddingValues,
+    viewModel: SymbolCreationViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+
+    val uiState by viewModel.uiState.collectAsState()
+    val categories by viewModel.categories.observeAsState(emptyList())
+
+    val isSymbolTextError = uiState.error?.type == SymbolCreationErrorType.SYMBOL_TEXT
+    val isCategoryError = uiState.error?.type == SymbolCreationErrorType.CATEGORY
+    val isPhotoInputError = uiState.error?.type == SymbolCreationErrorType.PHOTO_INPUT
+    val isError = isSymbolTextError || isCategoryError || isPhotoInputError
+
+    // Get photo from gallery
+    val galleryLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            viewModel.setPhotoInputUri(uri, context)
+        }
+
     Surface(
         modifier = modifier.fillMaxSize()
     ) {
-        Scaffold(
-            topBar = {
-                HomeTopAppBarUi(title = stringResource(id = R.string.symbol_creation))
-            }
-        ) { topPaddingValues ->
+        Scaffold(topBar = {
+            HomeTopAppBarUi(title = stringResource(id = R.string.symbol_creation))
+        }) { topPaddingValues ->
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -50,22 +98,146 @@ fun SymbolCreationScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 TitleUi(
-                    title = "상징 추가하기",
-                    description = "소개원실 좋아요"
+                    title = stringResource(id = R.string.create_symbol_text),
+                    description = stringResource(id = R.string.create_symbol_explain)
                 )
 
-                Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(30.dp))
 
-                OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .defaultMinSize(minHeight = 300.dp),
-                    textStyle = MaterialTheme.typography.bodyMedium,
-                    shape = RoundedCornerShape(10.dp)
+                AddPhotoButton(
+                    onClick = { galleryLauncher.launch("image/*") },
+                    isError = isPhotoInputError,
+                    viewModel = viewModel
+                )
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                // Symbol Text Field
+                TextFieldUi(
+                    value = viewModel.symbolTextInput,
+                    onValueChange = { viewModel.setSymbolText(it) },
+                    label = { Text(stringResource(R.string.new_symbol_name)) },
+                    supportingText = {
+                        if (isSymbolTextError) {
+                            Text(stringResource(id = uiState.error!!.messageId))
+                        }
+                    },
+                    isError = isSymbolTextError,
+                    isValid = uiState.isValidSymbolText
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                DropdownUi(
+                    selectedValue = viewModel.categoryInput,
+                    onValueChange = { viewModel.setCategory(it) },
+                    items = categories,
+                    modifier = Modifier.fillMaxWidth(),
+                    label = { Text(stringResource(R.string.category)) },
+                    isError = isCategoryError
+                )
+
+                ButtonUi(
+                    text = stringResource(id = R.string.register),
+                    onClick = { viewModel.createSymbol(context) },
+                    modifier = Modifier.offset(y = 50.dp),
+                    isEnabled = true,
+                    isError = false
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun DropdownUi(
+    selectedValue: Category?,
+    onValueChange: (Category) -> Unit,
+    items: List<Category>,
+    modifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null,
+    isError: Boolean = false
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .border(
+            1.dp,
+            if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.surfaceVariant,
+            RoundedCornerShape(10.dp)
+        )
+        .clickable { expanded = true }
+        .defaultMinSize(minHeight = 48.dp)
+        .padding(horizontal = 16.dp), contentAlignment = Alignment.Center) {
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            if (selectedValue != null) {
+                Text(
+                    text = selectedValue.text,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            } else {
+                label?.invoke() ?: Text(
+                    "Select an option", color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = stringResource(R.string.dropdown_icon_description)
+            )
+        }
+    }
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        modifier = Modifier
+            .width(300.dp)
+            .heightIn(max = 200.dp),
+    ) {
+        items.forEach { item ->
+            DropdownMenuItem(text = {
+                Text(
+                    text = item.text,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }, onClick = {
+                onValueChange(item)
+                expanded = false
+            })
+        }
+    }
+}
+
+@Composable
+private fun AddPhotoButton(
+    onClick: () -> Unit, isError: Boolean = false, viewModel: SymbolCreationViewModel
+) {
+    val color =
+        if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.surfaceVariant
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(120.dp)
+            .clickable { onClick() }
+            .border(
+                border = BorderStroke(1.dp, color), shape = RoundedCornerShape(10.dp)
+            )) {
+        if (viewModel.photoInputBitmap != null) {
+            // Display the loaded image if the photo input bitmap is not null
+            Image(
+                bitmap = viewModel.photoInputBitmap!!.asImageBitmap(),
+                contentDescription = stringResource(R.string.new_symbol_photo),
+                modifier = Modifier.fillMaxSize()
+            )
+        } else {
+            Icon(
+                painter = painterResource(id = R.drawable.outline_add_a_photo_24),
+                contentDescription = stringResource(R.string.symbol_creation)
+            )
         }
     }
 }
---
+++ frontend/app/src/main/java/com/example/speechbuddy/viewmodel/SymbolCreationViewModel.kt
@@ -0,0 +1,231 @@
+package com.example.speechbuddy.viewmodel
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.speechbuddy.R
+import com.example.speechbuddy.domain.models.Category
+import com.example.speechbuddy.domain.models.Symbol
+import com.example.speechbuddy.repository.SymbolRepository
+import com.example.speechbuddy.ui.models.SymbolCreationError
+import com.example.speechbuddy.ui.models.SymbolCreationErrorType
+import com.example.speechbuddy.ui.models.SymbolCreationUiState
+import com.example.speechbuddy.utils.isValidSymbolText
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.io.FileOutputStream
+import javax.inject.Inject
+
+@HiltViewModel
+class SymbolCreationViewModel @Inject internal constructor(
+    private val repository: SymbolRepository,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SymbolCreationUiState())
+    val uiState: StateFlow<SymbolCreationUiState> = _uiState.asStateFlow()
+
+    val categories = repository.getAllCategories().asLiveData()
+
+    var photoInputUri by mutableStateOf<Uri?>(null)
+
+    var photoInputBitmap by mutableStateOf<Bitmap?>(null)
+
+    var symbolTextInput by mutableStateOf("")
+        private set
+
+    var categoryInput by mutableStateOf<Category?>(null)
+        private set
+
+    @JvmName("callFromUri")
+    fun setPhotoInputUri(input: Uri?, context: Context) {
+        photoInputUri = input
+        if (_uiState.value.error?.type == SymbolCreationErrorType.PHOTO_INPUT) validatePhotoInput()
+        if (photoInputUri != null) photoInputBitmap = uriToBitmap(photoInputUri, context)
+    }
+
+    fun setSymbolText(input: String) {
+        symbolTextInput = input
+        if (_uiState.value.error?.type == SymbolCreationErrorType.SYMBOL_TEXT) validateSymbolText()
+    }
+
+    fun setCategory(input: Category) {
+        categoryInput = input
+        if (_uiState.value.error?.type == SymbolCreationErrorType.CATEGORY) validateCategory()
+    }
+
+    private fun validatePhotoInput() {
+        if (photoInputUri != null) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidPhotoInput = true,
+                    error = null
+                )
+            }
+        }
+    }
+
+    private fun validateCategory() {
+        if (categoryInput != null) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidCategory = true,
+                    error = null
+                )
+            }
+        }
+    }
+
+    private fun validateSymbolText() {
+        if (isValidSymbolText(symbolTextInput)) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidSymbolText = true,
+                    error = null
+                )
+            }
+        }
+    }
+    private fun clearInput() {
+        symbolTextInput = ""
+        categoryInput = null
+        photoInputUri = null
+        photoInputBitmap = null
+    }
+
+    @Suppress("DEPRECATION", "NewApi")
+    private fun uriToBitmap(uri: Uri?, context: Context): Bitmap {
+        return when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) { // version 28
+            true -> {
+                val source = ImageDecoder.createSource(context.contentResolver, uri!!)
+                ImageDecoder.decodeBitmap(source)
+            }
+
+            else -> {
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            }
+        }
+    }
+
+    fun bitmapToFile(context: Context, bitmap: Bitmap, fileName: String): File {
+        // Get the app's internal storage directory
+        val internalDir = context.filesDir
+        val imageFile = File(internalDir, "$fileName.png")
+
+        // Compress the bitmap as PNG and write it to the file
+        FileOutputStream(imageFile).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        }
+
+        return imageFile
+    }
+
+    private fun fileToMultipartBodyPart(file: File, paramName: String): MultipartBody.Part {
+        val requestFile = file.asRequestBody("image/*".toMediaType())
+        return MultipartBody.Part.createFormData(paramName, file.name, requestFile)
+    }
+
+    fun createSymbol(context: Context) {
+        if (!isValidSymbolText(symbolTextInput)) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidSymbolText = false,
+                    error = SymbolCreationError(
+                        type = SymbolCreationErrorType.SYMBOL_TEXT,
+                        messageId = R.string.invalid_new_symbol_text
+                    )
+                )
+            }
+        } else if (categoryInput == null) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidCategory = false,
+                    error = SymbolCreationError(
+                        type = SymbolCreationErrorType.CATEGORY,
+                        messageId = R.string.empty_category
+                    )
+                )
+            }
+        } else if (photoInputUri == null || photoInputBitmap == null) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isValidPhotoInput = false,
+                    error = SymbolCreationError(
+                        type = SymbolCreationErrorType.PHOTO_INPUT,
+                        messageId = R.string.empty_photo
+                    )
+                )
+            }
+        } else {
+            val id = (System.currentTimeMillis() % 10000 + 500).toInt()
+            val uniqueFileName = "symbol_${id}"
+            bitmapToFile(context, photoInputBitmap!!, uniqueFileName)
+            val symbol = Symbol(
+                id = id,
+                text = symbolTextInput,
+                imageUrl = null,
+                categoryId = categoryInput!!.id,
+                isFavorite = false,
+                isMine = true
+            )
+            viewModelScope.launch { repository.insertSymbol(symbol) }
+
+            clearInput()
+
+
+            // file processing
+//            val uniqueFileName = "symbol_${System.currentTimeMillis()}"
+//            val imageFile = bitmapToFile(context, photoInputBitmap!!, uniqueFileName)
+//            val imagePart = fileToMultipartBodyPart(imageFile, "image")
+//
+//            viewModelScope.launch(Dispatchers.IO) {
+//                repository.createSymbolBackup(
+//                    symbolText = symbolTextInput,
+//                    categoryId = categoryId,
+//                    image = imagePart
+//                ).collect{
+//                    if(it.status == Resource(Status.SUCCESS, "", "").status){
+//                        // Store new symbol in local db
+//                        val symbolId = it.data!!.id
+//                        val imageUrl = it.data!!.imageUrl
+//                        val symbol = Symbol(
+//                            id = 503,
+//                            text = "test",
+//                            imageUrl = "https://speechbuddy-bucket.s3.ap-northeast-2.amazonaws.com/media/symbol/user_25/202311081c9b0f83b83b4868912f11a4c4f81c85.jpeg",
+//                            categoryId = 1,
+//                            isFavorite = false,
+//                            isMine = true
+//                        )
+//                        repository.insertSymbol(symbol)
+//                    } else if (it.message?.contains("Unknown")==true){
+//                        _uiState.update { currentState ->
+//                            currentState.copy(
+//                                isValidSymbolText = false,
+//                                error = SymbolCreationError(
+//                                    type = SymbolCreationErrorType.SYMBOL_TEXT,
+//                                    messageId = R.string.internet_error
+//                                )
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+}
