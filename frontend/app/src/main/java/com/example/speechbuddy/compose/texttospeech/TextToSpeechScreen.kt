e10b244282750b5f78519b01f852d1365ea0bd9c
Seungmin Joo <prime2do@gmail.com>
2023-11-15 17:55:26 +0900

    :fire: Remove preview code

--- frontend/app/src/main/java/com/example/speechbuddy/compose/texttospeech/TextToSpeechScreen.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/compose/texttospeech/TextToSpeechScreen.kt
@@ -34,13 +34,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.speechbuddy.R
 import com.example.speechbuddy.compose.utils.HomeTopAppBarUi
 import com.example.speechbuddy.compose.utils.TitleUi
-import com.example.speechbuddy.ui.SpeechBuddyTheme
 import com.example.speechbuddy.ui.models.ButtonStatusType
 import com.example.speechbuddy.viewmodel.TextToSpeechViewModel
 
@@ -247,15 +245,5 @@ fun TextClearButton(
                 tint = deactivatedColor
             )
         }
-    }
-}
-
-@Preview
-@Composable
-fun TextToSpeechScreenPreview() {
-    SpeechBuddyTheme {
-        TextToSpeechScreen(
-            bottomPaddingValues = PaddingValues(16.dp)
-        )
     }
 }
