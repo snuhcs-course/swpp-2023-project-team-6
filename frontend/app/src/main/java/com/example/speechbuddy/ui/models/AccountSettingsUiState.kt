53ee7d149a0a88032fda76821e2d7c41a29dd318
89645321 <13619@snu.ac.kr>
2023-11-16 10:39:31 +0900

    Update frontend/app/src/main/java/com/example/speechbuddy/ui/models/AccountSettingsUiState.kt
    Co-authored-by: YeonJeong Kim <65206075+yjeong-k@users.noreply.github.com>

--- frontend/app/src/main/java/com/example/speechbuddy/ui/models/AccountSettingsUiState.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/ui/models/AccountSettingsUiState.kt
@@ -3,10 +3,6 @@ package com.example.speechbuddy.ui.models
 import com.example.speechbuddy.domain.models.User
 
 data class AccountSettingsUiState(
-    /**
-     * TODO
-     * email, nickname은 나중에 user 모델 하나로 묶어야 할 듯
-     */
     val user: User? = null,
     val alert: AccountSettingsAlert? = null,
 )
