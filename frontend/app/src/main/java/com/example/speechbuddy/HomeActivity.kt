8d5f80ed537d2d1364adc05aab1c1e19e926713d
MyungHyun Ryu <paulryu@snu.ac.kr>
2023-11-20 00:17:19 +0900

    :sparkles: Auto hide keyboard when screen clicked

--- frontend/app/src/main/java/com/example/speechbuddy/HomeActivity.kt
+++ frontend/app/src/main/java/com/example/speechbuddy/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.example.speechbuddy
 
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.example.speechbuddy.compose.SpeechBuddyHome
 import com.example.speechbuddy.ui.SpeechBuddyTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity() {
@@ -36,6 +41,18 @@ class HomeActivity : BaseActivity() {
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
+    }
+    // hides keyboard
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v != null) {
+                v.clearFocus()
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 
 }
