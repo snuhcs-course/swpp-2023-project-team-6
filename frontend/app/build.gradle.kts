5c91548095f941d4aa43a6c5f2a921c0d8d2d598
Seungmin Joo <prime2do@gmail.com>
2023-11-20 03:11:29 +0900

    :test_tube: Work on photo handling

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
