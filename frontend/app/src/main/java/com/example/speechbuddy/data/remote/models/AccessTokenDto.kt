05d86805452b2a96296b456b0fb702860a2081ab
Sukchan Lee <scleev@snu.ac.kr>
2023-11-22 17:51:56 +0900

    :sparkles: Implement display menu switch in symbol selection screen

--- frontend/app/src/main/java/com/example/speechbuddy/data/remote/models/AccessTokenDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.speechbuddy.data.remote.models
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class AccessTokenDto(
-    @Json(name = "access") val accessToken: String? = null
-)
--- frontend/app/src/main/java/com/example/speechbuddy/ui/models/BackupSettingsUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.speechbuddy.ui.models
-
-data class BackupSettingsUiState(
-    /* TODO */
-    val lastBackupDate: String = "2023.09.09",
-    val isAutoBackupEnabled: Boolean = true
-)
